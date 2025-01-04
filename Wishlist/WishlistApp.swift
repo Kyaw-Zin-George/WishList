8d0ed81c19cc2fdf67d2d7e85a1b60e1a4ab6fd9
Kyaw-Zin-George <ktz1695@gmail.com>
2025-01-04 19:58:05 +0000

    Initial Commit

---
+++ Wishlist/WishlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  WishlistApp.swift
+//  Wishlist
+//
+//  Created by Kyaw Thant Zin(George) on 1/4/25.
+//
+
+import SwiftUI
+
+@main
+struct WishlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
