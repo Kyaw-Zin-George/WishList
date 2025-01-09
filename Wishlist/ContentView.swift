d8cbb27165c16a6696900e5f52170eedc98af9f4
Kyaw-Zin-George <ktz1695@gmail.com>
2025-01-09 13:51:02 +0000

    Creating a full list sample preview by creating a container  within the particular preview

--- Wishlist/ContentView.swift
+++ Wishlist/ContentView.swift
@@ -16,6 +16,8 @@ struct ContentView: View {
             List{
                 ForEach(wishes){ wish in
                     Text(wish.title)
+                        .font(.title.weight(.light))
+                        .padding(.vertical, 2)
                 }
             }//:List
             .navigationTitle("Wishes")
@@ -28,7 +30,18 @@ struct ContentView: View {
     }
 }
 
-#Preview {
+#Preview("Full List") {
+    let container = try! ModelContainer(for: Wish.self,configurations: ModelConfiguration(isStoredInMemoryOnly: true))
+                                        
+    container.mainContext.insert(Wish(title: "Master SwiftDatat"))
+    container.mainContext.insert(Wish(title: "Practice SwiftUI"))
+    container.mainContext.insert(Wish(title: "Learn SwiftUI"))
+    
+     return ContentView()
+        .modelContainer(container)
+}
+
+#Preview("Empty List") {
     ContentView()
         .modelContainer(for: Wish.self,inMemory: true)
 }
